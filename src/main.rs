9362fcc1cd548a73785366601d19cbdc5f2be7bd
ypocritique <simon.nana@yahoo.com>
2026-04-23 23:34:45 +0200

    Added changes to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::result;
 
 
 
 
+                     
+
+// the same code with mutable variables     
 fn main() {
-    println!("Hello, world!");
+    let proceed = true;
+    if proceed {
+        println!("Hello, WSL!");
+    } else {
+        println!("Goodbye, WSL!");
+    }
 
+    let height = 1.95;
+    if height > 1.85 {
+        "You are tall!"
+    } else if height == 1.85 {
+       "You are of average height!"
+    } else {                                
+        "You are short!"
+    };
+
+    println!("Result: {}", height);
 }
